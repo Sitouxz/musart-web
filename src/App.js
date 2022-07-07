eff2fe8fae4e5d28cbdd957bec6f442b64d3ae5e
im-Starry <s2200054@student.unklab.ac.id>
2022-07-07 16:23:38 +0800

    griffin merg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
+    <div className='App'>
+      <header className='App-header'></header>
+      <h1 className='text-3xl font-bold underline'>Hello world</h1>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        {" "}
+        Click Me
+      </button>
+      <h1>asdflkj</h1>
     </div>
   );
 }
